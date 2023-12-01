bfef339d888d08db6ab5b5f3db5845da7301080d
Niyaaz Daniels <niyaazdaniels1@gmail.com>
2023-12-01 12:26:21 +0200

    send

--- js/script.js
+++ js/script.js
@@ -5,15 +5,12 @@ let searchBtn = document.getElementById('search').addEventListener('click', ()=>
 let output = document.querySelector('#output')
 
 // array containing names 
-const randomName = ['Yuti', 'Peter', 'Steve', 'Eve', 'X', 'Max', 'Igi', 'Alex', 'Brad', 'Zeke', 'Lee', 'Harry',
-                'Luther', 'Carl', 'Dave', 'Fred', 'Wale', 'Nigel', 'Que', 'Richard' ];
+const randomName = [' Yuti ', ' Peter ', ' Steve ', ' Eve ', ' X ', ' Max ', ' Igi ', ' Alex ', ' Brad ', ' Zeke ', ' Lee ', ' Harry ',
+                ' Luther ', ' Carl ', ' Dave ', ' Fred ', ' Wale ', ' Nigel ', ' Que ', ' Richard ' ];
 
 // function to display names
 function displayName (){
-    let x = randomName.slice(' ');
-    for(let x of randomName){
         output.textContent  = randomName;
-    }
 }
 // function toggle
 function ascending(){
